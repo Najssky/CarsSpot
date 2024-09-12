3fa8278eb31f2f9e85d46176236c545e9d396f8d
Najssky <sokolowski.m98@gmail.com>
2024-09-12 17:38:53 +0200

    Add Tailwind, create structure and components

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,16 @@
+<template>
+  <nav-component></nav-component>
+</template>
+
+<script>
+import NavComponent from "./NavComponent.vue"
+export default {
+  components: { NavComponent },
+  name: "HeaderComponent",
+  props: {},
+}
+</script>
+
+<style>
+
+</style>
